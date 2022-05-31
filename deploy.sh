1e8fe2028d9096287dffeb5d9d142d9c03cb1329
Matheus Streb Vieira <mataca9@gmail.com>
2022-05-31 09:54:02 -0300

    removing dist and and adding deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:mataca9/mataca9.github.io.git main
+
+cd -
